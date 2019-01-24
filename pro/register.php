741ecab097f12b52aaba435ae16656d4dbf7e38a
ahmedyounas <mahmedyounas@gmail.com>
2019-01-24 15:07:26 +0500

    quiz 8 (product search)

--- pro/register.php
+++ pro/register.php
@@ -11,24 +11,26 @@ require "server/functions.php";
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
-    <script>
-        function checkEmail(str) {
+
+        <script>
+            function checkEmail(str) {
             if (str.length == 0) {
-                document.getElementById("hint").innerHTML = "";
-                return;
-            } else {
-                var xmlhttp = new XMLHttpRequest();
-                xmlhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("hint").innerHTML = this.responseText;
-                    }
-                };
-                xmlhttp.open("GET", "check_email.php?e=" + str, true);
-                xmlhttp.send();
-                //document.getElementById('hint').innerHTML = 'loading...';
-            }
+            document.getElementById("hint").innerHTML = "";
+            return;
+        } else
+        {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("hint").innerHTML = this.responseText;
+        }
+        };
+            xmlhttp.open("GET", "search.php?e=" + str, true);
+            xmlhttp.send();
+        }
         }
     </script>
+
 </head>
 <body>
 
